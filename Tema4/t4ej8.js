4d89d03e2dce089fea9ee279ddf9dd102038cc25
Roberto Domínguez <201902@CALASANZSALAMANCA.local>
2020-10-14 12:02:50 +0200

    saqgd

---
+++ Tema4/t4ej8.js
@@ -0,0 +1,9 @@
+function numAleatorio(){
+    let aleatorio;
+    let valor1 = 1;
+    let valor2 = 2;
+
+    aleatorio = Math.random() * (valor2 - valor2) + valor1;
+    aleatorio = Math.round(aleatorio);
+    return aleatorio; 
+}
